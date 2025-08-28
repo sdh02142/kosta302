1207327824ba23131eb3a3455249c5d114c7832e
sdh02142 <sdh02142@naver.com>
2025-08-28 14:45:25 +0900

    add MemberController.java

--- src/exam/view/MemberController.java
+++ src/exam/view/MemberController.java
@@ -1,6 +1,6 @@
 package exam.view;
 
-public class MainView {
+public class MemberController {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
--- src/exam/view/MenuView.java
+++ src/exam/view/MenuView.java
@@ -4,5 +4,7 @@ public class MainView {
 
 	public static void main(String[] args) {
 		System.out.println("Bye22");
+		System.out.println("Bye22");
+		System.out.println("Bye22");
 	}
 }
