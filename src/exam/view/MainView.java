fd5105d1e35895a965c795ad46deb78849184615
sdh02142 <sdh02142@naver.com>
2025-08-28 14:32:11 +0900

    mod 2files

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -4,5 +4,6 @@ public class MainView {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Local Modification");
 	}
 }
--- src/exam/view/MenuView.java
+++ src/exam/view/MenuView.java
@@ -4,5 +4,6 @@ public class MainView {
 
 	public static void main(String[] args) {
 		System.out.println("Bye22");
+		System.out.println("Bye22");
 	}
 }
