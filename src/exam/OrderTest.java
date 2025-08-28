636b8510def8052aa18f32c845117c152d7e1aab
sdh02142 <sdh02142@naver.com>
2025-08-28 17:09:56 +0900

    first commit, mod OrderTest.java

--- src/exam/OrderTest.java
+++ src/exam/OrderTest.java
@@ -4,5 +4,8 @@ public class OrderTest {
 
 	public static void main(String[] args) {
 		System.out.println("Bye22");
+		System.out.println("");
+		System.out.println("Bye22");
+		System.out.println("Bye22");
 	}
 }
