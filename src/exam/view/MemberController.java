0b56120682f38ba79a202bea931d4257484fb5b7
sdh02142 <sdh02142@naver.com>
2025-08-28 14:47:10 +0900

    Update Origin Main MemberController.java

--- src/exam/view/MemberController.java
+++ src/exam/view/MemberController.java
@@ -6,5 +6,6 @@ public class MemberController {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
