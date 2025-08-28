d86f484e9c860a1b94be52455803c0d2fcdbc089
sdh02142 <sdh02142@naver.com>
2025-08-28 15:35:32 +0900

    Update MainView.java 2

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -6,5 +6,6 @@ public class MainView {
 		System.out.println("Hello World");
 		System.out.println("Nice To Meet You");
 		System.out.println("Is this right, World?");
+		System.out.println("Everything is okay");
 	}
 }
